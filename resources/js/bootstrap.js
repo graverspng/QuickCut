f752f9987e92ad8599048f260818f1d2861c456e
graverspng <126756242+graverspng@users.noreply.github.com>
2025-10-16 13:15:04 +0300

    bootstrap.js & app.blade.php +1

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const tokenElement = document.head.querySelector('meta[name="csrf-token"]');
+if (tokenElement) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = tokenElement.content;
+} else {
+    console.warn('CSRF token not found: uploads and POST requests may fail.');
+}
