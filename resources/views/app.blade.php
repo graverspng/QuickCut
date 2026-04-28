8a50c2f68e3f62fdd4c335b3a15c7c070c128702
graverspng <126756242+graverspng@users.noreply.github.com>
2026-04-28 13:42:29 +0300

    Vite::asset('resources/js/Pages/Auth/img/QuickCut.png')

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Favicon (loaded via Vite, just like your login image) -->
-        <link rel="icon" type="image/png" href="{{ Vite::asset('resources/js/Pages/Auth/img/QuickCut.png') }}">
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/img/QuickCut.png">
 
         <!-- Scripts -->
         @routes
